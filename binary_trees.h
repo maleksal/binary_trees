55abbf981419cee659d0963df45174dbd5e30a32
Taha Yazidi <yazi.taha@gmail.com>
2020-07-23 18:27:30 +0000

    header_file

---
+++ binary_trees.h
@@ -0,0 +1,31 @@
+#ifndef binary_tree
+#define binary_tree
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Binary Tree Structure */
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+
+/* FUnction's prototypes */
+void binary_tree_print(const binary_tree_t *);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+#endif
