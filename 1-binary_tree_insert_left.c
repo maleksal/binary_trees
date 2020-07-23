a1404a1da8e4ab57933aef99306ffb1531da43f6
Malek Salem <malek.salem.14@gmail.com>
2020-07-23 21:23:06 +0000

    fix betty style

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -32,6 +32,5 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		new_left_node->left = temp_node_holder;
 		temp_node_holder->parent = new_left_node;
 	}
-	return new_left_node;
-	
+	return (new_left_node);
 }
