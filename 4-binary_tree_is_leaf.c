1f7ec4cfbef055ea5c64f3dcc014581e50952a0c
Malek Salem <malek.salem.14@gmail.com>
2020-07-23 22:20:16 +0000

    fix red check 7

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -9,7 +9,7 @@
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	/* check right nd left nodes */
-	if (!node->left && !node->right)
+	if (node && (!node->left && !node->right))
 		return (1);
 	return (0);
 
