264ecdd0610a8fb98e8d3b4931209067dbb39f2c
Malek Salem <malek.salem.14@gmail.com>
2020-07-23 21:09:32 +0000

    add helper function

--- binary_trees.h
+++ binary_trees.h
@@ -27,5 +27,6 @@ typedef struct binary_tree_s binary_tree_t;
 /* FUnction's prototypes */
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+void binary_tree_print(const binary_tree_t *);
 
 #endif
