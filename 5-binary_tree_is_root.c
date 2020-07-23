e0f84ceb8cf8aeb377e67b3e211a2a9b7e2ae598
Malek Salem <malek.salem.14@gmail.com>
2020-07-23 22:32:39 +0000

    fix/update code

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -9,10 +9,5 @@
 int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node && !node->parent)
-	{
-		if ((node->left->parent == node || !node->left)
-				&& (node->right->parent == node || !node->right))
-			return (1);
-	}
-	return (0);
-}
+		return (0);
+	return (1);
