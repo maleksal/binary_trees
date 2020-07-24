ba0a98b63e5ca7ada59c69aad3377092e83ade7e
Malek Salem <malek.salem.14@gmail.com>
2020-07-24 19:43:49 +0000

    add task 6

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - travers a tree in preorder way then passes
+ * the node value to func pointer
+ * @tree: pointer to binary tree
+ * @func: pointer to function
+ */
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree)
+		return;
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+}
