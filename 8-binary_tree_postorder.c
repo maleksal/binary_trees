692af01df11cb868c599d1c1e36b89b6baf4e9c6
Taha Yazidi <yazi.taha@gmail.com>
2020-07-24 20:33:30 +0000

    postorder_traversing

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - traverse binary tree with postorder.
+ * @tree: root pointer.
+ * @func: pointer to a function.
+ * Return: nothing.
+ */
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+	{
+		return;
+	}
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
+}
