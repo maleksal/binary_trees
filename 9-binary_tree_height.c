1de546561e2564378ab8f4fe6defa7049859df19
Taha Yazidi <yazi.taha@gmail.com>
2020-07-26 08:38:46 +0000

    binT_height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,14 +8,16 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t lh = 0;
+	size_t rh = 0;
 
 	if (!tree || (!tree->left && !tree->right))
 	{
 		return (0);
 	}
 
-	size_t lh = binary_tree_height(tree->left);
-	size_t rh = binary_tree_height(tree->right);
+	lh = binary_tree_height(tree->left);
+	rh = binary_tree_height(tree->right);
 
 	if (lh > rh)
 	{
